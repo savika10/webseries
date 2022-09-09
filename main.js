1ebb544b05ef45a166370d123aeb2d541bbb2946
savika10 <62184652+savika10@users.noreply.github.com>
2022-09-09 09:18:55 +0530

    Assignment 2

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//using ES5
+//using ES6
 
 class WebSeries {
   constructor(title, director, stars, streamingPlatform) {
@@ -7,10 +7,194 @@ class WebSeries {
     this.stars = stars;
     this.streamingPlatform = streamingPlatform;
   }
+  get title() {
+    return this._title.toUpperCase();
+  }
+  get director() {
+    return this._director;
+  }
+  get stars() {
+    return this._stars;
+  }
+  get streamingPlatform() {
+    return this._streamingPlatform;
+  }
+  set title(newTitle) {
+    if (newTitle == "") {
+      throw "Title cannot be empty";
+    }
+    this._title = newTitle;
+  }
+  set director(newDirector) {
+    if (newDirector == "") {
+      throw "Director cannot be empty";
+    }
+    this._director = newDirector;
+  }
+  set stars(newStars) {
+    if (newStars == "") {
+      throw "stars cannot be empty";
+    }
+    this._stars = newStars;
+  }
+  set streamingPlatform(newStreamingPlatform) {
+    if (newStreamingPlatform == "none") {
+      throw "Streaming Platform cannot be empty";
+    }
+    this._streamingPlatform = newStreamingPlatform;
+  }
 }
 
+const array = [
+  new WebSeries("x1", "y1", "z1", "s1"),
+  new WebSeries("x2", "y2", "z2", "s2"),
+  new WebSeries("x3", "y3", "z3", "s3"),
+  new WebSeries("x4", "y4", "z4", "s4"),
+  new WebSeries("x5", "y5", "z5", "s5"),
+  new WebSeries("x6", "y6", "z6", "s6"),
+];
 class UI {
-  constructor() {}
+  //constructor() {}
+  addWebSeriesToList(WebSeries) {
+    console.log(WebSeries);
+    const list = document.getElementById("Web-Series-list");
+    //create
+    const span = document.createElement("span");
+    console.log(span);
+    span.className = "card";
+    //insert
+    span.innerHTML = `<h2>${WebSeries.title}</h2>
+    <p>${WebSeries.director}</p>
+    <p>${WebSeries.stars}</p>
+    <p>${WebSeries.streamingPlatform}</p>
+    <button><a href="#" class="delete">DELETE</a></button>`;
+
+    list.appendChild(span);
+  }
+  deleteList(target) {
+    return new Promise(function (resolve, reject) {
+      setTimeout(function () {
+        if (target.className === "delete") {
+          target.parentElement.parentElement.remove();
+        }
+        resolve();
+      }, 5000);
+    });
+  }
+
+  clearFields() {
+    document.getElementById("title").value = "";
+    document.getElementById("director").value = "";
+    document.getElementById("stars").value = "";
+    document.getElementById("streamingPlatform").value = "none";
+  }
+}
+
+for (var value of array) {
+  console.log(
+    value.title,
+    value.director,
+    value.stars,
+    value.streamingPlatform
+  );
+}
+function formsubmit(e) {
+  e.preventDefault();
+  const title = document.getElementById("title").value,
+    director = document.getElementById("director").value,
+    stars = document.getElementById("stars").value,
+    streamingPlatform = document.getElementById("streamingPlatform").value;
+  const value = new WebSeries(title, director, stars, streamingPlatform);
+  console.log(
+    value.title,
+    value.director,
+    value.stars,
+    value.streamingPlatform
+  );
+  const ui = new UI();
+  ui.addWebSeriesToList(value);
+  //clear fields
+  ui.clearFields();
+}
+
+var form = document.querySelector("form");
+form.addEventListener("submit", formsubmit);
+
+document
+  .getElementById("Web-Series-list")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    const ui = new UI();
+    ui.deleteList(e.target);
+    //alert
+  });
+/*form.addEventListener(
+  "submit",
+  function debugEventStoppages({
+    methods = ["stopPropagation", "stopImmediatePropagation", "preventDefault"],
+    type = "log",
+    should = () => true,
+  } = {}) {
+    methods.forEach((method) => {
+      const original = Event.prototype[method];
+      Event.prototype[method] = function () {
+        // Test if we need do log/break for this particular event
+        if (should(this)) {
+          if (type == "break") {
+            debugger;
+          } else {
+            // Log which method was called, the event type,
+            // the event itself and a stacktrace to figure out what happened
+            console.log(
+              method,
+              this.type,
+              this,
+              (() => {
+                try {
+                  throw new Error();
+                } catch (e) {
+                  return e;
+                }
+              })()
+            );
+          }
+        }
+        return original.apply(this, arguments);
+      };
+    });
+  }
+);
+//document.addEventListener("submit", console.log, true);
+/*document.addEventListener(
+  "submit",
+  function (event) {
+    console.log(
+      event.type, // The type of the event
+      event.target, // The target of the event
+      event, // The event itself
+      (() => {
+        try {
+          throw new Error();
+        } catch (e) {
+          return e;
+        }
+      })() // A stacktrace to figure out what triggered the event
+    );
+  },
+  true
+);
+/*document.addEventListener(
+  "submit",
+  function () {
+    debugger;
+  },
+  true
+);
+document.addEventListener("submit", console.log, true);
+*/
+
+/*class UI extends WebSeries {
+  //constructor() {}
   addWebSeriesToList(WebSeries) {
     console.log(WebSeries);
     const list = document.getElementById("Web-Series-list");
@@ -38,7 +222,7 @@ class UI {
 document
   .getElementById("Web-series-form")
   .addEventListener("submit", function (e) {
-    const tile = document.getElementById("title").value,
+    const title = document.getElementById("title").value,
       director = document.getElementById("director").value,
       stars = document.getElementById("stars").value,
       streamingPlatform = document.getElementById("streamingPlatform").value;
